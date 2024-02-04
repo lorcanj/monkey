b2523c240ef758fdeb1af17b4e1da53603b14287
Lorcan <lorcanjames1@gmail.com>
2024-02-04 16:38:26 +0000

    update tests and lexer to include greater range of keywords etc

--- monkey/token/token.go
+++ monkey/token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"   // 123456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	EXCLAM   = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimeters
 	COMMA     = ","
@@ -31,4 +38,29 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIndent(ident string) TokenType {
+	// this checks whether ident is in the keywords map
+	// if it is then return the TokenType for that input
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	// else return the TokenType token.IDENT used for all user-defined ids
+	return IDENT
+}
